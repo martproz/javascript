09a0782801937e910b214d257711baeeeabab064
martproz <martinprozapas@gmail.com>
2022-02-07 15:58:47 -0300

    for

--- script.js
+++ script.js
@@ -5,16 +5,21 @@ let meses = parseFloat(prompt("Ingrese la cantidad de meses"))
 let resultado = valorCuotas(deuda, CFT, meses) 
 console.log(`El valor de cada cuota es de ${resultado}`)
 
-while (deuda >= 200) {
+if (deuda >= 200) {
     alert("Su deuda es elevada");
-    break
 }
 
-while (CFT >= 300) {
+if (CFT >= 300) {
     alert("Nuestro banco quebrará por gente como usted.");
-    break
 }
 
+for(meses = 1; meses === 1; meses--) {
+    console.log("Usted ha consultado por " + meses + " mes.");
+}
+
+for(meses = 2; meses <= 12; meses++) {
+    console.log("Usted ha consultado por " + meses + " meses.");
+}
 
 
 
