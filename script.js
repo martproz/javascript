7c16898d9c5e8d6528e91cc72a5cfda8d68ce543
Martín Prozapas <93625544+martproz@users.noreply.github.com>
2022-03-02 14:12:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
 class Cliente {
-    constructor(nombre, apellido, deuda, cft, meses, resultado) {
+    constructor(nombre, apellido, email, deuda, cft, meses, resultado) {
         this.nombre = nombre;
         this.apellido = apellido;
+        this.email = email;
         this.deuda = deuda;
         this.cft = cft;
         this.meses = meses;
@@ -12,74 +13,79 @@ class Cliente {
 
 let arrayClientes = []
 
-if(localStorage.getItem('clientes')) {
-    arrayClientes = JSON.parse(localStorage.getItem('clientes'))
+
+if(localStorage.getItem('usuarios')) {
+    arrayClientes = JSON.parse(localStorage.getItem('usuarios'))
 } else {
-    localStorage.setItem('clientes', JSON.stringify(arrayClientes))
+    localStorage.setItem('usuarios', JSON.stringify(arrayClientes))
 }
-    
-    let formDcmr = document.getElementById("formDcmr")
-    let botonConsultarClientes = document.getElementById("botonConsultarClientes")
-    let divClientes = document.getElementById("divClientes")
 
-    formDcmr.addEventListener('botonConsultarClientes', (e) => {
-        e.preventDefault()
-    
-        let nombre = document.getElementById('nombre').value
-        let apellido = document.getElementById('apellido').value
-        let deuda = document.getElementById('deuda').value
-        let cft = document.getElementById('cft').value
-        let meses = document.getElementById('meses').value
-    
-    
-        if(!arrayClientes.some(clienteEnArray => clienteEnArray.apellido == apellido)) {
-            const cliente = new Cliente(nombre, apellido, deuda, cft, meses)
-            arrayClientes.push(cliente)
-            localStorage.setItem('clientes', JSON.stringify(arrayClientes))
-            formDcmr.reset()
-        }
-        
-    })
 
-    botonConsultarClientes.addEventListener('click', () => {
-        arrayClientes.forEach((clienteEnArray, indice)  => {
-            divClientes.innerHTML += `
-                <div class="card" id="user${indice}" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Nombre ${clienteEnArray.nombre}</h5>
-                        <p class="card-text">Apellido: ${clienteEnArray.apellido}</p>
-                        <p class="card-text">Apellido: ${clienteEnArray.deuda}</p>
-                        <p class="card-text">Apellido: ${clienteEnArray.cft}</p>
-                        <p class="card-text">Apellido: ${clienteEnArray.meses}</p>
-                        <button id="boton${indice}" class="btn btn-danger">Eliminar</button>
-                    </div>
+let formulario = document.getElementById("idForm")
+let botonMostrarUsuarios = document.getElementById("botonMostrarUsers")
+let divUsers = document.getElementById("divUsuarios") 
+
+
+function IsEmpty() {
+    if (document.forms['frm'].nombre.value === "") {
+      console.log("Por favor, complete los campos");
+      return false;
+    } else if (document.forms['frm'].apellido.value === "") {
+    return false;
+  } else if (document.forms['frm'].email.value === "") {
+    return false;
+} else if (document.forms['frm'].deuda.value === "") {
+    return false;
+} else if (document.forms['frm'].cft.value === "") {
+    return false;
+} else if (document.forms['frm'].meses.value === "") {
+    return false;
+} else {
+    return true
+}
+}
+
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault()
+    e.IsEmpty()
+
+    let nombre = document.getElementById('idNombre').value; 
+    let apellido = document.getElementById('idApellido').value; 
+    let email = document.getElementById('idEmail').value;
+    let deuda = parseInt(document.getElementById('idDeuda').value); 
+    let cft = parseInt(document.getElementById('idCft').value);
+    let meses = parseInt(document.getElementById('idMeses').value);
+    let resultado = (+deuda + +cft) / meses; 
+    document.getElementById("resultadoTexto").value = resultado;
+
+    });
+
+
+    if(!arrayClientes.some(usuarioEnArray => usuarioEnArray.email == email)) {
+        const cliente = new Cliente(nombre, apellido, email, deuda, cft, meses, resultado)
+        arrayClientes.push(cliente)
+        localStorage.setItem('usuarios', JSON.stringify(arrayClientes))
+        formulario.reset()
+    }
+
+
+botonMostrarUsuarios.addEventListener('click', () => {
+    arrayClientes.forEach((usuarioEnArray, indice)  => {
+        divUsers.innerHTML += `
+            <div class="card" id="user${indice}" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title col">Usuario: ${usuarioEnArray.nombre}</h5>
+                    <p class="card-text col">Apellido: ${usuarioEnArray.apellido}</p>
+                    <p class="card-text col">Email: ${usuarioEnArray.email}</p>
+                    <p class="card-text col">Deuda: ${usuarioEnArray.deuda}</p>
+                    <p class="card-text col">CFT: ${usuarioEnArray.cft}</p>
+                    <p class="card-text col">Meses: ${usuarioEnArray.meses}</p>
+                    <p class="card-text col">Cuota mensual: ${usuarioEnArray.resultado}</p>
+                    <button id="boton${indice}" class="btn btn-danger">Eliminar</button>
                 </div>
-            
-            `
-        }) //cierre del forEach
-    }) //cierre del CLICK
-
-    function valorCuotas(){ 
-
-        let nombre = document.getElementById('nombre').value; 
-    
-        let apellido = document.getElementById('apellido').value; 
-    
-        let nomAP = nombre + "" + apellido
-    
-        let deuda = parseInt(document.getElementById('deuda').value); 
-    
-        let CFT = parseInt(document.getElementById('cft').value); 
-    
-        let meses = document.getElementById('meses').value;
-    
-        var resultado = (+deuda + +CFT) / meses; 
-    
-        document.getElementById("resultadoTexto").value = resultado;
-    
-        console.log(nomAP)
-    
-        }
-    
-    
-
+            </div>
+        
+        `
+    })
+})
